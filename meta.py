cd0bd7048b9cc887deffff01316fe140d6e7108a
Dhoushini S <90917829+Dhoushinisengottaiyan@users.noreply.github.com>
2023-05-27 09:42:29 +0530

    Update meta.py

--- meta.py
+++ meta.py
@@ -9,21 +9,17 @@ SIDEBAR_INFO = """
 CHEF_INFO = """
 <h2 class="font-title">Welcome to  Crazy  Restaurant! </h2>
 <p class="strong font-body">
-<span class="d-block extra-info">(We are at your service with two of the best chefs in the world: Chef Ragavi, 
-Chef Dhoushini. Ragavi is known for being more creative whereas Dhoushini is more meticulous.)</span>
+<span class="d-block extra-info">(We are at your service with two of the best chefs in the world: Chef Traditional, 
+Chef Modern. Moedern is known for being more creative whereas Traditiional is more meticulous.)</span>
 </p>
 """.strip()
 PROMPT_BOX = "Add custom ingredients here (separated by `,`): "
 STORY = """
 <div class="story-box font-body">
 <p>
-Hello everyone 👋, I am <strong>Chef Dhamu</strong>, 
-the owner of this restaurant.  
-I've never been more proud of my students -- both can produce exceptional dishes but I regard Jayes as being <em>creative</em> while Pradhap is <em>meticulous</em>. 
-If you give each of them the same ingredients, they'll usually come up with something different. <br /><br />
-At the start of the program the chefs read cookbooks containing thousands of recipes of varying difficulties and from cultures all over the world. 
-The NLP engineers helped guide the learning process so that the chefs could actually learn which ingredients work well together rather than just memorize recipes. 
-I trained my chefs by asking them to generate a title, a list of ingredients (including amounts!), and a list of directions after giving them just a simple list of food items. 
+Hello everyone 👋, I am <strong>Maaster Chef </strong>, 
+the owner of this restaurant.  Ask me anything about cooking
+
 </p>
 
 <pre>[Inputs]
